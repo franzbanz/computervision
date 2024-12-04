6fa09cb67a358d42e2ee677de8c1981664908280
franzbanz <franz@koehler-kn.de>
2024-12-04 14:50:52 +0100

    new testcase, tweaked text detection parameters

--- text_detection_generalized_no_visuals.py
+++ text_detection_generalized_no_visuals.py
@@ -23,7 +23,7 @@ def create_ocr_reader(languages):
     """Create an OCR reader object."""
     return easyocr.Reader(languages) # set gpu=False if you want to use CPU
 
-def extract_text_from_image(reader, image, text_threshold=0.7):
+def extract_text_from_image(reader, image, text_threshold=0.5):
     """Extract text from an image using the OCR reader."""
     return reader.readtext(image, text_threshold=text_threshold)
 
@@ -63,17 +63,17 @@ def filter_forbidden_chars(detections, forbidden_chars):
     return [detection for detection in detections if not (len(detection[1]) == 1 and detection[1] in forbidden_chars)]
 
 # Execution
-target_image_path = 'images/testcase10.png' # testcase5.png good for vertical text, testcase3.png good for horizontal text
+target_image_path = 'images/testcase11.png' # testcase5.png good for vertical text, testcase3.png good for horizontal text
 target_img, original_shape = load_image(target_image_path)
 easyocr_img = target_img.copy()
 forbidden_chars = ['<','.','>',',','/',';',':','\'','"','[',']','{','}','|','\\','`','~','!','^','(',')','_','-','+','=']
 
 reader = create_ocr_reader(['en'])
-result = extract_text_from_image(reader, easyocr_img, text_threshold=0.0)
+result = extract_text_from_image(reader, easyocr_img, text_threshold=0.1)
 
 # Rotate the image by 90 degrees counterclockwise and search for text
 rotated_img = rotate_image(easyocr_img, 90)
-rotated_result = extract_text_from_image(reader, rotated_img, text_threshold=0.0)
+rotated_result = extract_text_from_image(reader, rotated_img, text_threshold=0.1)
 
 # Adjust the coordinates of the rotated text and combine results
 result = adjust_and_combine_results(result, rotated_result, easyocr_img.shape, 90)
